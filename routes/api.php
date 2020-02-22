ec2209aef811c9f5561b944c29510bc3c4de27d1
龙张 <callme-z@qq.com>
2020-02-23 00:00:36 +0800

    no message

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+
+/*
+ * Backend Routes
+ * Namespaces indicate folder structure
+ */
+Route::group(['namespace' => 'Frontend/Api', 'prefix' => 'api', 'as' => 'api.'], function () {
+    /*
+     * These routes need view-backend permission
+     * (good if you want to allow more than one group in the backend,
+     * then limit the backend features by different roles or permissions)
+     *
+     * Note: Administrator has all permissions so you do not have to specify the administrator role everywhere.
+     * These routes can not be hit if the password is expired
+     */
+    include_route_files(__DIR__.'/api/frontend/');
+});

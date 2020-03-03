31445595b47c7790d6365dd570890529ff788930
龙张 <callme-z@qq.com>
2020-03-03 18:05:50 +0800

    no message

--- app/Helpers/Global/SystemHelper.php
+++ app/Helpers/Global/SystemHelper.php
@@ -38,3 +38,28 @@ if (! function_exists('include_route_files')) {
         include_files_in_folder($folder);
     }
 }
+
+
+if (! function_exists('settings')) {
+    /**
+     * Get / set the specified configuration value.
+     *
+     * If an array is passed as the key, we will assume you want to set an array of values.
+     *
+     * @param  array|string|null  $key
+     * @param  mixed  $default
+     * @return mixed|\App\Settings\Repository
+     */
+    function settings($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('settings');
+        }
+
+        if (is_array($key)) {
+            return app('settings')->set($key);
+        }
+
+        return app('settings')->get($key, $default);
+    }
+}
